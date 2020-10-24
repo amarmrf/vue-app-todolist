d470d3d99649581c0c5e8f9bbf80a9fe21d7cc24
Amar Maruf <amarmarf@gmail.com>
2020-10-24 21:46:19 +0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
+
+
+/*
+import React from "react";
+
+class Min extends React.Component {
+  render() {
+    return (
+    <div>
+      
+    </div>
+  )
+}
+}
+
+export default Min
+*/
+
+/*
+https://jsonplaceholder.typicode.com/todos
+*/
+
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <br/>
+    <h2>This is a Vue.js TodoList app v1.0.0</h2>
+    <br/>
+    <p>Made by <a href="http://www.github.com/amarmrf" target="_blank"> Amar </a></p>
+  </div>
+</template>
